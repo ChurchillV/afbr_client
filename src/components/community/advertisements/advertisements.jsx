b67d1b34d0b7e2a9588ef7c317ea0edc027c44f1
ChurchillV <104873546+ChurchillV@users.noreply.github.com>
2022-10-21 15:20:17 +0000

    Some changes to component imports

--- src/components/community/advertisements/advertisements.jsx
+++ src/components/community/advertisements/advertisements.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import axios from 'axios'
 import Navbar from '../../navbar';
-import ads_image from '../images/ads.png'
 import { Community_Nav } from '../community_nav'
 import Footer from '../../footer'
-import Ads_Effect from './ads_message' 
-import No_Ads_Yet from './noadsyet';
+import AdsEffect from './ads_message' 
+import NoAdsYet from './noadsyet';
+
 
 
 
@@ -28,8 +28,8 @@ function advertisements() {
     <>
       <Navbar/>
       <Community_Nav/>
-      <Ads_Effect/>
-      <No_Ads_Yet/>
+      <AdsEffect/>
+      <NoAdsYet/>
       <Footer/>
     </>  
   )
